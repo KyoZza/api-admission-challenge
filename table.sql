bd5e544fd50a9003c61c3bf8ce59fdd868e05237
KyoZza <sandro.scheffmann@hotmail.de>
2021-11-12 00:13:02 +0900

    possible fix to 'DEPTH_ZERO_SELF_SIGNED_CERT' ?

--- table.sql
+++ table.sql
@@ -1,50 +1,50 @@
--- DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS recipes;
 
--- CREATE TABLE IF NOT EXISTS recipes (
---   id SERIAL PRIMARY KEY,
---   -- name of recipe
---   title varchar(100) NOT NULL,
---   -- time required to cook/bake the recipe
---   making_time varchar(100) NOT NULL,
---   -- number of people the recipe will feed
---   serves varchar(100) NOT NULL,
---   -- food items necessary to prepare the recipe
---   ingredients varchar(300) NOT NULL,
---   -- price of recipe
---   cost integer NOT NULL,
---   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
---   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
--- );
+CREATE TABLE IF NOT EXISTS recipes (
+  id SERIAL PRIMARY KEY,
+  -- name of recipe
+  title varchar(100) NOT NULL,
+  -- time required to cook/bake the recipe
+  making_time varchar(100) NOT NULL,
+  -- number of people the recipe will feed
+  serves varchar(100) NOT NULL,
+  -- food items necessary to prepare the recipe
+  ingredients varchar(300) NOT NULL,
+  -- price of recipe
+  cost integer NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
--- CREATE FUNCTION update_updated_at_column() RETURNS trigger
---     LANGUAGE plpgsql
---     AS $$
---   BEGIN
---     NEW.updated_at = CURRENT_TIMESTAMP;
---     RETURN NEW;
---   END;
--- $$;
+CREATE FUNCTION update_updated_at_column() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+  BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+  END;
+$$;
 
--- CREATE TRIGGER recipes_updated_at_modtime BEFORE UPDATE ON recipes FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+CREATE TRIGGER recipes_updated_at_modtime BEFORE UPDATE ON recipes FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
 
--- INSERT INTO recipes (
---   title,
---   making_time,
---   serves,
---   ingredients,
---   cost,
---   created_at,
---   updated_at
--- )
--- VALUES (
---   'Chicken Curry',
---   '45 min',
---   '4 people',
---   'onion, chicken, seasoning',
---   1000,
---   '2016-01-10 12:10:12',
---   '2016-01-10 12:10:12'
--- );
+INSERT INTO recipes (
+  title,
+  making_time,
+  serves,
+  ingredients,
+  cost,
+  created_at,
+  updated_at
+)
+VALUES (
+  'Chicken Curry',
+  '45 min',
+  '4 people',
+  'onion, chicken, seasoning',
+  1000,
+  '2016-01-10 12:10:12',
+  '2016-01-10 12:10:12'
+);
 
 INSERT INTO recipes (
   title,
